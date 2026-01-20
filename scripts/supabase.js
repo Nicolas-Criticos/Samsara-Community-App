631fbd6e1378ba6742eabe177469bce5ad83f415
Nicolas Criticos <1622717@students.wits.ac.za>
2026-01-20 13:56:46 +0100

    Use env variables for Supabase config

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,9 +1,9 @@
 // scripts/supabase.js
 // scripts/supabase.js
 
-const SUPABASE_URL = "https://recqyhjooukkdwsrjslp.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlY3F5aGpvb3Vra2R3c3Jqc2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMjkzMTAsImV4cCI6MjA4MzcwNTMxMH0.vN0bzTBBrtSBzicjniXXyCUAdn9YIWKHmXIKMJKb0rg";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 
 export const supabaseClient = supabase.createClient(
   SUPABASE_URL,
